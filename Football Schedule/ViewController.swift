63a4592359f120b31ad4e05e2d8974952b666b27
Tony Albor <tonyalbor92@gmail.com>
2019-01-13 00:21:03 -0800

    Initial Commit

---
+++ Football Schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Football Schedule
+//
+//  Created by Tony Albor on 1/13/19.
+//  Copyright © 2019 Tony Albor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
