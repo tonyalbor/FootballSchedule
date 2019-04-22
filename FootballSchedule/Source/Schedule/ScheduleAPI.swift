47f0bcd31697cc646c101f47a97f0afb89188976
Tony Albor <tonyalbor92@gmail.com>
2019-04-21 22:22:18 -0700

    look up matches by matchday

--- FootballSchedule/Source/Schedule/ScheduleAPI.swift
+++ FootballSchedule/Source/Schedule/ScheduleAPI.swift
@@ -48,17 +48,49 @@ final class ScheduleAPI {
     
     private let api = APIClient<Response>()
     private let competitionAPI = CompetitionAPI()
-    private let database = Database<[Match]>()
+    private let matchesDatabase = Database<[Match]>()
+    private let seasonDatabase = Database<Season>()
     private(set) var matches = [Match]()
     
     private static let currentMatchesKey = "currentMatches"
     
+    func seasonKey(competitionCode: String) -> String {
+        return "currentSeason:\(competitionCode)"
+    }
+    
+    func matchesKey(competitionCode: String, matchday: Int) -> String {
+        return "matches:\(competitionCode):\(matchday)"
+    }
+    
     func getCurrentMatches(competitionCode: String, completion: @escaping (Result<[Match]>) -> Void) {
-        if let currentMatches = database.get(key: ScheduleAPI.currentMatchesKey) {
-            matches = currentMatches
-            // TODO: check the matches to see if they've all been played. if so, hit network instead of returning these old matches
-            completion(.success(currentMatches))
-            return
+        // figure out if stored matchday is still active:
+        // go through the stored matches for that matchday
+        // look for the matches that don't have final scores
+        // look for any match that current date is later than start date
+        // if there is even just one - this matchday is stale
+//        if let currentMatchday = matchdayDatabase.get(key: "matchday:\(competitionCode)") {
+//            if let currentMatches = readMatches(competitionCode: competitionCode, matchday: currentMatchday) {
+//                // all done?
+//                let dontHaveFinalScores = currentMatches.filter { (match) -> Bool in
+//                    return match.score.fullTime?.awayTeam == nil || match.score.fullTime?.homeTeam == nil
+//                }
+//
+//            }
+//        }
+//        if let currentMatches = matchesDatabase.get(key: ScheduleAPI.currentMatchesKey) {
+//            matches = currentMatches
+//            // TODO: check the matches to see if they've all been played. if so, hit network instead of returning these old matches
+//            completion(.success(currentMatches))
+//            return
+//        }
+        let currentSeasonKey = seasonKey(competitionCode: competitionCode)
+        if let currentSeason = seasonDatabase.get(key: currentSeasonKey) {
+            let currentMatchesKey = matchesKey(competitionCode: competitionCode, matchday: currentSeason.currentMatchday)
+            if let currentMatches = matchesDatabase.get(key: currentMatchesKey) {
+                matches = currentMatches
+                completion(.success(currentMatches))
+                return
+            }
         }
         Log.verbose("hitting competition API for current matches")
         competitionAPI.getCompetition(code: competitionCode) { [weak self] result in
@@ -68,8 +100,8 @@ final class ScheduleAPI {
                     competitionCode: competitionCode,
                     matchday: response.currentSeason.currentMatchday,
                     completion: { [weak self] result in
-                        if case let .success(matches) = result {
-                            self?.database.save(record: matches, key: ScheduleAPI.currentMatchesKey)
+                        if case .success = result {
+                            self?.seasonDatabase.save(record: response.currentSeason, key: currentSeasonKey)
                         }
                         completion(result)
                     })
@@ -80,11 +112,17 @@ final class ScheduleAPI {
     }
     
     func getMatches(competitionCode code: String, matchday day: Int, completion: @escaping (Result<[Match]>) -> Void) {
+        let key = matchesKey(competitionCode: code, matchday: day)
+        if let value = matchesDatabase.get(key: key) {
+            completion(.success(value))
+            return
+        }
         let request = Request(competitionCode: code, matchday: day)
         api.request(request) { [weak self] response in
             let result = response.map { $0.matches }
             if case let .success(value) = result {
                 self?.matches = value
+                self?.matchesDatabase.save(record: value, key: key)
             }
             completion(result)
         }
